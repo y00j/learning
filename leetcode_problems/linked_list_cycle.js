d45fa1f79f97f4b825cd9bbe391400ec87c5ed68
Yujie Zhu <yujiezhu718@gmail.com>
2018-07-26 16:48:52 -0700

    finished inefficient linked list hasCycle method

---
+++ leetcode_problems/linked_list_cycle.js
@@ -0,0 +1,40 @@
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val) {
+ *     this.val = val;
+ *     this.next = null;
+ * }
+ */
+
+/**
+ * @param {ListNode} head
+ * @return {boolean}
+ */
+var hasCycle = function(head) {
+  if (head === null || head.next === null) return false;
+  let nodeList = [];
+  let curr = head;
+  let temp;
+  while (curr) {
+    
+       if ( nodeList.includes(curr) ) {
+           
+          return true;
+      }
+      nodeList.push(curr);
+      curr = curr.next;
+     
+
+  }
+  
+  return false;
+  
+  function _contains(array, node) {
+      for( let i = 0; i < array.length; i++) {
+          if (array[i] === node) {
+              return true;
+          }
+      }
+      return false;
+  }
+};
